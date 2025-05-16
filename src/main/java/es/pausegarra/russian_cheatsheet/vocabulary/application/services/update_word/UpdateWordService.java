b9237143c8c6eaa1054ce162304c04bf2a845ebe
psegarra <pausegarra.b@gmail.com>
2025-05-16 21:48:57 +0200

    refacor: remove conjugations if updated word is not a verb

--- src/main/java/es/pausegarra/russian_cheatsheet/vocabulary/application/services/update_word/UpdateWordService.java
+++ src/main/java/es/pausegarra/russian_cheatsheet/vocabulary/application/services/update_word/UpdateWordService.java
@@ -33,8 +33,8 @@ public class UpdateWordService implements Service<UpdateWordDto, Void> {
         .toEntity()
         .withWord(word)
         .withId(word.getConjugations().getId());
-    }else {
-      word = word.withConjugations(null);
+    } else {
+      conjugations = null;
     }
 
     WordEntity updated = word.update(
